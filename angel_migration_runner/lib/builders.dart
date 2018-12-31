438cf80f931f6c17dd174dc12608d11783a72505
Tobe O <thosakwe@gmail.com>
2018-12-31 08:36:47 -0500

    auto cleanup migration info

--- angel_migration_runner/lib/builders.dart
+++ angel_migration_runner/lib/builders.dart
@@ -1,4 +1,9 @@
 import 'package:build/build.dart';
 import 'src/builders/migration_info.dart';
 
-Builder migrationInfoBuilder([BuilderOptions options]) => MigrationInfoBuilder();
+Builder migrationInfoBuilder([BuilderOptions options]) =>
+    MigrationInfoBuilder();
+
+PostProcessBuilder migrationInfoCleanup(BuilderOptions options) =>
+    FileDeletingBuilder(['.migration_info.json'],
+        isEnabled: options.config['enabled'] == true);
